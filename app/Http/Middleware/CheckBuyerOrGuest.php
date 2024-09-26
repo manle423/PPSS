3ee9cbc5ea5712a38d59441df7fbe49a7a0e61c1
manle423 <trungmanle.vm@gmail.com>
2024-09-26 15:28:48 +0700

    merge

--- app/Http/Middleware/CheckBuyerOrGuest.php
+++ app/Http/Middleware/CheckBuyerOrGuest.php
@@ -17,14 +17,25 @@ class CheckBuyerOrGuest
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->role === User::BUYER) {
-            return $next($request);
+        // Kiểm tra xem người dùng đã đăng nhập hay chưa
+        if (Auth::check()) {
+            $user = Auth::user();
+            
+            // Kiểm tra nếu người dùng có role là 'admin'
+            if ($user->role === User::ADMIN) {
+                return redirect()->route('admin.dashboard'); // Chuyển hướng tới trang admin
+            }
+
+            // Kiểm tra nếu người dùng có role là 'buyer'
+            if ($user->role === User::BUYER) {
+                return $next($request); // Tiếp tục xử lý request
+            }
+
+            // Nếu role không phải admin hoặc buyer, có thể trả về trang lỗi hoặc chuyển hướng khác
+            abort(404);
         }
 
-        if (!Auth::check()) {
-            return $next($request);
-        }
-        abort(404);
-        // return redirect(route('admin.dashboard'));
+        // Nếu không đăng nhập, tiếp tục cho phép truy cập với vai trò khách
+        return $next($request);
     }
 }
