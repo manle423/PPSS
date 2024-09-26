261ef7250f70b0994ac2d0e8736f1456c097d213
chidiep2312 <chidiep23122003@gmail.com>
2024-09-26 23:14:48 +0700

    thay đoi footer

--- resources/views/webshop/home.blade.php
+++ resources/views/webshop/home.blade.php
@@ -5,7 +5,56 @@
     <div class="container-fluid py-5 mb-5 hero-header">
     </div>
     <div class="container-fluid fruite py-5">
-  
+    <div class="container py-4">
+            <div class="row g-4">
+                <div class="col-md-6 col-lg-3">
+                    <div class="featurs-item text-center rounded bg-light p-4">
+                       
+                            <i class="fas fa-car-side fa-3x" style="color: #81c408;"></i>
+                      
+                        <div class="featurs-content text-center">
+                            <h5 class="title-text">Free ship</h5>
+                            <p class="mb-0">Orders over 500,000 VND</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="featurs-item text-center rounded bg-light p-4">
+                      
+                            <i class="fas fa-user-shield fa-3x" style="color: #81c408;""></i>
+                    
+                        <div class="featurs-content text-center">
+                            <h5 class="title-text">Secure Payment</h5>
+                            <p class="mb-0">100% Safe Guarantee</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="featurs-item text-center rounded bg-light p-4">
+                    
+                            <i class="fas fa-exchange-alt fa-3x"style="color: #81c408;"></i>
+                      
+                        <div class="featurs-content text-center">
+                            <h5 class="title-text" >Refund</h5>
+                            <p class="mb-0">7 Days Money Back Guarantee </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="featurs-item text-center rounded bg-light p-4">
+                      
+                    <i class="fa fa-phone-alt fa-3x" style="color: #81c408;"></i>
+
+                        
+                        <div class="featurs-content text-center">
+                            <h5 class="title-text">Support</h5>
+                            <p class="mb-0">24/7 customer support</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
                
        
    
@@ -27,22 +76,22 @@
                             <li class="nav-item">
                                 <a class="d-flex m-2 py-2 bg-light rounded-pill active" data-bs-toggle="pill"
                                     href="#tab-1">
-                                    <span class="text-dark" style="width: 130px;">All Products</span>
+                                    <span class="text-dark" style="width: 130px;text-align:center">All Products</span>
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="d-flex py-2 m-2 bg-light rounded-pill" data-bs-toggle="pill" href="#tab-2">
-                                    <span class="text-dark" style="width: 130px;">Clothes</span>
+                                    <span class="text-dark" style="width: 130px;text-align:center">Clothes</span>
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="d-flex m-2 py-2 bg-light rounded-pill" data-bs-toggle="pill" href="#tab-3">
-                                    <span class="text-dark" style="width: 130px;">Toys</span>
+                                    <span class="text-dark" style="width: 130px; text-align:center">Toys</span>
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="d-flex m-2 py-2 bg-light rounded-pill" data-bs-toggle="pill" href="#tab-4">
-                                    <span class="text-dark" style="width: 130px;">Foods</span>
+                                    <span class="text-dark" style="width: 130px;text-align:center">Foods</span>
                                 </a>
                             </li>
 
