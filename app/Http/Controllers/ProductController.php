5f9b1032f8bc35fbe4fe32d63121ab13ff440a88
manle423 <trungmanle.vm@gmail.com>
2024-10-03 06:57:47 +0700

    save

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -56,22 +56,6 @@ class ProductController extends Controller
         return view('product.shop', compact('products', 'categories'));
     }
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(Request $request, Product $product)
@@ -115,31 +99,5 @@ class ProductController extends Controller
 
         return view('product.shop-detail', ['product' => $product, 'variants' => $variants,'categories' => $categories]);
         //return view('product.show', ['product' => $product, 'variants' => $variants]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        // $product = Product::findOrFail($id);
-        // $product->update($request->all());
-        // return response()->json($product);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
